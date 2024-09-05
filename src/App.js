71905372dc1fc8868def199d7b6aedde13dc3882
Kanishka Saket <kanishkasaket1007@gmail.com>
2024-09-05 18:51:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { initialState, reducer, Provider } from "./state";
+
+import { RootLayout } from "./layouts";
+import { HomePage, QuizPage, ResultPage, RulesPage, PageNotFound } from "./pages";
+
+import "./App.css";
+
+const App = () => {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	const renderElement = (element, navigateTo) => {
+		if (state.user.name !== "" && state.user.email !== "") {
+			return element;
+		}
+		return <Navigate to={navigateTo} />
+	}
+
+	return (
+		<Provider value={{ state, dispatch }}>
+			<Routes>
+				<Route path="/" element={<RootLayout />}>
+					<Route index element={<HomePage />} />
+					<Route path="rules" element={renderElement(<RulesPage />, "/")} />
+					<Route path="quiz" element={renderElement(<QuizPage />, "/")} />
+					<Route path="result" element={renderElement(<ResultPage />, "/")} />
+				</Route>
+
+				<Route path="*" element={<PageNotFound />} />
+			</Routes>
+		</Provider>
+	);
+};
 
 export default App;
